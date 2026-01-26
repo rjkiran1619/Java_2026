189bbe1fdffd7d5121a5d79bd3653bf51ef78561
Raj Kiran <rjkiran1619@gmail.com>
2026-01-26 12:44:56 +0530

    swap programs

---
+++ src/swap.java
@@ -0,0 +1,17 @@
+// WAP to swap number without using temp file.//
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 20;
+
+a = a + b;
+b = a - b;
+a = a - b;
+System.out.println("a value is :" +a);
+System.out.println("b value is : " +b);
+	}
+
+}
---
+++ src/swapII.java
@@ -0,0 +1,17 @@
+
+public class swapII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a  = 30;
+int b = 60;
+int temp;
+temp = a;
+a = b;
+b = temp;
+System.out.println(a);
+System.out.println(b);
+
+	}
+
+}
